c01e9f6eabf73670be6757a3cc5815ad6186e05e
ChadDiaz <chadcdiaz@gmail.com>
2020-11-22 23:31:22 -0500

    added answers to readme

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,87 +1,99 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 // import { Switch, Route } from 'react-router-dom'
 
-import { connect } from 'react-redux';
-import { Button } from 'react-bootstrap';
+import { connect } from "react-redux";
+import { Button } from "react-bootstrap";
 import "./App.css";
 import SmurfList from "./SmurfList";
-import { addSmurf } from '../store/actions'
+import { addSmurf } from "../store/actions";
+import styled from "styled-components";
+
+const FormDiv = styled.form`
+  display: inline-flex;
+  flex-direction: column;
+  width: 300px;
+
+`;
 
 const App = (props) => {
+  const [newSmurf, setNewSmurf] = useState([
+    {
+      name: "",
+      age: 0,
+      height: "",
+    },
+  ]);
 
-  const [newSmurf, setNewSmurf] = useState([{
-    name: '',
-    age: 0,
-    height: '',
-    
-  }])
-  
   const handleChange = (e) => {
-    const value = e.target.name === "age, height" ? Number(e.target.value): e.target.value;
-    setNewSmurf({...newSmurf, [e.target.name]: value})
-  }
+    const value =
+      e.target.name === "age, height" ? Number(e.target.value) : e.target.value;
+    setNewSmurf({ ...newSmurf, [e.target.name]: value });
+  };
 
   const handleClick = (e) => {
     e.preventDefault();
     props.addSmurf(newSmurf);
     setNewSmurf({
-      name: '',
+      name: "",
       age: null,
       height: null,
-    })
-  }
+    });
+  };
 
   return (
     <div className="App">
-      
       <h1>SMURFS!</h1>
       <h2>Click below to see the list of Smurfs Already in the Database:</h2>
-      <SmurfList /> 
+      <SmurfList />
       <h2>Fill out this form to add a new Smurf!</h2>
-      <form>
-        
-        <label htmlFor="name"/>Name
-        <input 
-        id='name'
-        name='name'
-        placeholder="Enter Name Here"
-        value={newSmurf.name}
-        onChange={handleChange}
+      <FormDiv>
+        <label htmlFor="name" />
+        Name
+        <input
+          id="name"
+          name="name"
+          placeholder="Enter Name Here"
+          value={newSmurf.name}
+          onChange={handleChange}
         />
-        <br/>
-        <br/>
-        <label htmlFor="age"/>Age
-        <input 
-        id='age'
-        name='age'
-        placeholder="Enter Age Here"
-        value={newSmurf.age}
-        onChange={handleChange}
+        <br />
+        <br />
+        <label htmlFor="age" />
+        Age
+        <input
+          id="age"
+          name="age"
+          placeholder="Enter Age Here"
+          value={newSmurf.age}
+          onChange={handleChange}
         />
-        <br/>
-        <br/>
-        <label htmlFor="height"/>Height in cm
-        <input 
-        id='height'
-        name='height'
-        placeholder="Enter Height Here"
-        value={newSmurf.height}
-        onChange={handleChange}
+        <br />
+        <br />
+        <label htmlFor="height" />
+        Height in cm
+        <input
+          id="height"
+          name="height"
+          placeholder="Enter Height Here"
+          value={newSmurf.height}
+          onChange={handleChange}
         />
-        <br/>
-        <br/>
+        <br />
+        <br />
         <button onClick={handleClick}>Click Here to Add Your Smurf</button>
-      </form>
+        <br/>
+        <br/>
+      </FormDiv>
     </div>
   );
 };
 
-const mapStateToProps = ({SR}) => {
+const mapStateToProps = ({ SR }) => {
   return {
     smurfs: SR.smurfs,
     isLoading: SR.isLoading,
     error: SR.error,
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, {addSmurf})(App);
+export default connect(mapStateToProps, { addSmurf })(App);
